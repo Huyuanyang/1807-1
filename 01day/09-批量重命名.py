709c2051eaa2ec5db0910bf6098659538f27c7a0
赵庆元 <qingyuan@geekniu.com>
2018-08-06 17:25:21 +0800

    文件

---
+++ 01day/09-批量重命名.py
@@ -0,0 +1,12 @@
+import os
+dir = input("请输入批量重命名的文件夹名字")
+files = os.listdir(dir)#列表
+#os.chdir(dir)
+#print(os.getcwd())
+for i in files:
+    position = i.rfind(".")
+    newname = i[:position]+"-阿里"+i[position:]
+    oldname = dir+"/"+i#movie/吃鸡.py
+    newname = dir+"/"+newname
+    os.rename(oldname,newname)
+
